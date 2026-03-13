75dcaa8bb4b36043c5d255984df3a14248de0ed0
Pranay Kalekar <pranaykalekar20@gmail.com>
2026-03-13 21:08:37 +0530

    Backend with hardcoded cloud DB

--- Travlling website SQL/config/sql.js
+++ Travlling website SQL/config/sql.js
@@ -1,14 +1,40 @@
+// const { Sequelize } = require("sequelize");
+
+// const sequelize = new Sequelize(
+//   "travel_sql",
+//   "root",
+//   "pranay1123",   
+//   {
+//     host: "localhost",
+//     dialect: "mysql",
+//     timezone: "+05:30"
+//   }
+// );
+
+// module.exports = sequelize;
+
+
+
+// १. आधी Sequelize इंपोर्ट करा (हे विसरू नकोस!)
 const { Sequelize } = require("sequelize");
 
+// २. मग डोकं लावून हार्डकोड माहिती टाकूया (Aiven ची)
 const sequelize = new Sequelize(
-  "travel_sql",
-  "root",
-  "pranay1123",   
+  "defaultdb", 
+  "avnadmin", 
+  "AVNS_wss1Nhd4UmN4QhLGFWg", 
   {
-    host: "localhost",
+    host: "mysql-78a2a13-pranaykalekar20-bce1.f.aivencloud.com",
+    port: 13916,
     dialect: "mysql",
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false // Aiven साठी हे खूप महत्त्वाचं आहे
+      }
+    },
     timezone: "+05:30"
   }
 );
 
+// ३. शेवटी एक्सपोर्ट करा
 module.exports = sequelize;
